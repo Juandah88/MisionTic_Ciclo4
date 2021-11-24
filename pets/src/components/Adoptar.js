3109352db8ceedba9be168786ce218296bd972d7
Guillermo Ramirez <garamirez1994@gmail.com>
2021-11-24 08:20:30 -0500

    Adoptar
    Creacion de tarjetas

--- pets/src/components/Adoptar.js
+++ pets/src/components/Adoptar.js
@@ -1,50 +1,47 @@
-import React, { Component } from 'react';
-import Foter from './Footer';
-import axios from 'axios';
-
-export default class Adoptar extends Component {
+import React, { useEffect, useState } from "react";
+import Api from "../helpers/Conector.js";
+import axios from "axios";
 
 
-    get = () => {
-        axios
-          .get("http://localhost:5000/api/mascotas")
-          .then((response) => {
-            this.setState({ pets: response.data });
-          })
-          .catch((error) => {
-            console.log(error.message);
-          });
-      };
 
-    render() {
-        return (
+const Adoptar = () => {
+  const [state, setState] = useState([]);
+  const [isLoding, setIsLoding] = useState(true);
 
-            <div className="fondo">
-            <h1>Página adoptar</h1>
+  useEffect(() => {
+  
+    axios.get(Api + "/mascotas").then((res) => {
+      setState(res.data);
+      
+    });
+      setIsLoding(false);
+  }, []);
+  
+  return (
+    <div className="ContainerMain">
 
-        <div class="row row-cols-1 row-cols-md-3 g-4 m-5">
+      <h1>Página adoptar</h1>
+     
+       <div class="row row-cols-1 row-cols-md-3 g-4 m-5">
+       {state.map((pet) => (
         <div class="col">
-        {
-            this.state.pets.map(pet =>
-            <div class="card h-100">
-            <img src="..." class="card-img-top" alt="..."></img>
-            <div class="card-body">
-                <h5 class="card-title">{ pet.nombre }</h5>
-                <p class="card-text">{ pet.perfil }</p>
-            </div>
-            <div class="card-footer">
-                <small class="text-muted">{ pet.raza } - { pet.edad }</small>
-            </div>
-            </div>
-            )
-        }
-        </div>
+         <div class="card h-100">
+         <img
+          src="https://ichef.bbci.co.uk/news/640/amz/worldservice/live/assets/images/2015/06/12/150612173542_mascota_promo_624x351_thinkstock.jpg" class="card-img-top" alt="..."></img>
+         <div class="card-body">
+             <h5 class="card-title">{ pet.nombre }</h5>
+             <p class="card-text">{ pet.perfil }</p>
+             <a class="btn btn-success"> ver mas...</a>
+         </div>
+         <div class="card-footer" bgcolor="white">
+             <small class="text-muted">Raza: { pet.raza } - meses: { pet.edad }</small>
+         </div>
+         </div>
+       </div>
+       ))}
+      </div>
+    </div>
+  );
+};
 
-        </div>
-            <Foter/>
-        </div>
-
-        );
-    }
-
-}
+export default Adoptar;

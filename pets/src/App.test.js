f5b6f24b4348cd6db70facd481988780826ac149
tiag0777 <89233127+tiag0777@users.noreply.github.com>
2021-11-30 15:39:41 -0500

    modificaciones

--- pets/src/App.test.js
+++ pets/src/App.test.js
@@ -12,3 +12,18 @@ var nav = document.getElementById('nav');
 menuhamburguesa.addEventListener('click', function(){
   nav.classList.toggle('mostrar');
 })
+
+function irariba(pxPatantalla){
+  window.addEventListener('scroll', () =>{
+    var scroll = document.documentElement.scrollTop;
+    console.log(scroll);
+    var botonarriba = document.getElementById('botonarriba');
+    
+    if(scroll > 300){
+      botonarriba.style.right= 20 + "px";
+    }else{
+      botonarriba.style.right= -100 + "px";
+    }
+  })
+}
+irariba();
